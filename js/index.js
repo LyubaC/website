fac013e090e6d53239c30647f63fd79839c9b572
Lyubov Chepil <lyubac@ukr.net>
2018-08-10 20:26:45 +0000

    small ui fixes

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
             
     function addNews(item) {
         $("#news").append(
-           "<article class='news-item col-3'><a href='"+item.url+"'><h3>"+ item.title +"</h3>"+
+           "<article class='news-item col-12 col-sm-6 col-lg-4 col-xl-3'><a href='"+item.url+"'><h3>"+ item.title +"</h3>"+
             "<img src=\"" + item.urlToImage + "\" /><p>"+ item.description +"</p></a></article>"
         ); 
     }
     function loadNews(about) {
-        $.ajax({url: "https://newsapi.org/v2/everything?q=" + about + "&apiKey=c82dd1db265e4e06a92deefffcf65c71", success: function(result){
+        $.ajax({url: "https://newsapi.org/v2/everything?q=" + about + "&language=uk&from=2018-08-01&apiKey=c82dd1db265e4e06a92deefffcf65c71", success: function(result){
             //$("#news").html(result);
             let newsJSON = result;
             if (newsJSON.status == "ok") {
